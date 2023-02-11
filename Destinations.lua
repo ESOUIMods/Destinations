a6563d3fe629228fc54d148913e6c42a11c93174
Sharlikran <danielhmpdx16@gmail.com>
2023-02-10 17:02:27 -0800

    Version: 29.84

--- Destinations.lua
+++ Destinations.lua
@@ -122,7 +122,7 @@ end
 -------------------------------------------------
 
 local ADDON_AUTHOR = "|c990000Snowman|r, |cFFFFFFDK|r, Ayantir, MasterLenman, |cFF9B15Sharlikran|r"
-local ADDON_VERSION = "29.83"
+local ADDON_VERSION = "29.84"
 local ADDON_WEBSITE = "http://www.esoui.com/downloads/info667-Destinations.html"
 
 local LMP = LibMapPins
